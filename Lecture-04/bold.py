888af09e67b450aa83ab85c9ac49edcaa3d53c4a
natthanicha291 <s6806022610011@email.kmutnb.ac.th>
2025-07-15 15:04:13 +0700

    L-4-2

---
+++ Lecture-04/bold.py
@@ -0,0 +1,5 @@
+rows = int(input("How many rows: "))
+columns = int(input("How many columns: "))
+
+for _ in range (rows):
+    print( '*' * columns)
---
+++ Lecture-04/sentinel.py
@@ -0,0 +1,5 @@
+keep_going = 'y'
+
+while keep_going == "y":
+    price = float(input('Enter the item is wholesale cost: '))
+    
