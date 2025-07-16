ac7b7bc5eff48732141c34f52673a625a9b04e62
natthanicha291 <s6806022610011@email.kmutnb.ac.th>
2025-07-16 07:13:54 +0000

    L-04-h

--- Lecture-04/sentinel.py
+++ Lecture-04/sentinel.py
@@ -2,4 +2,12 @@ keep_going = 'y'
 
 while keep_going == "y":
     price = float(input('Enter the item is wholesale cost: '))
+    # another = float(input('Enter the item is wholesale cost:'))
+
+    retail_price = price * 2.5
+
+    print(f'The retail price is: ${retail_price:.2f}')
+
+    keep_going = input('Do you have another item? (y/n): ')
+
     
