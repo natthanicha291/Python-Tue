1238339363fb5a63fcfdeecdade06b31642dfe3d
natthanicha291 <s6806022610011@email.kmutnb.ac.th>
2025-07-28 14:01:32 +0000

    le-06-1

--- Lecture-06/acc_list.py
+++ Lecture-06/acc_list.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 print(f"prime_numbers:, {prime_numbers}")
 fifth_prime = prime_numbers[4]
 print(f" fifth prime number: {fifth_prime}")
+=======
+prime_numbers = [2, 3, 5, 7, 11]
+>>>>>>> ab7b930 (le-06-1)
--- Lecture-06/list_ex.py
+++ Lecture-06/list_ex.py
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 fruits = ["apple","banana","cherry"]
 numbers = [1, 2, 3, 4, 5]
 mixed_list = ["apple", 1, 2.5, True]
+=======
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4,5]
+mixed = ["apple", 1, 2.5, True]
+>>>>>>> ab7b930 (le-06-1)
